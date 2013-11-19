237b53ea9bf49817c63e838d85e466db2d62ddd3
unknown <cbotto@ualberta.ca>
2013-11-19 16:33:05 -0700

    First commit, working create and destroy database with some test code under menu option '2' to show key/data pairs

---
+++ src/Create.java
@@ -0,0 +1,119 @@
+import java.util.Random;
+
+import com.sleepycat.db.*;
+
+
+
+public class Create {
+
+    // to specify the file name for the table
+	//NOTE: I HARDCODED MY USERNAME. CHANGE AT WILL
+    public static final String BTREE_TABLE = "/tmp/cbotto_db/btree_table";
+    public static final String HASH_TABLE = "/tmp/cbotto_db/hash_table";
+    private static final int NO_RECORDS = 1000;
+    public String type;
+    public Database my_table;
+	
+    public Create(String type) {
+    	this.type = type;
+    	this.my_table = null;
+    }
+    
+	
+	public void createDatabase() {
+		try {
+
+		    // Create the database object.
+		    // There is no environment for this simple example.
+		    DatabaseConfig dbConfig = new DatabaseConfig();
+		    if (this.type.equals("btree")) {
+			    dbConfig.setType(DatabaseType.BTREE);
+			    dbConfig.setAllowCreate(true);
+			    this.my_table = new Database(BTREE_TABLE, null, dbConfig);
+			    System.out.println(BTREE_TABLE + " has been created");
+
+			    /* populate the new database with NO_RECORDS records */
+			    populateTable(my_table,NO_RECORDS);
+			    System.out.println("1000 records inserted into" + BTREE_TABLE);
+	    	}
+	    	else if (this.type.equals("hash")) {
+	    		dbConfig.setType(DatabaseType.HASH);
+			    dbConfig.setAllowCreate(true);
+			    this.my_table = new Database(HASH_TABLE, null, dbConfig);
+			    System.out.println(HASH_TABLE + " has been created");
+
+			    /* populate the new database with NO_RECORDS records */
+			    populateTable(my_table,NO_RECORDS);
+			    System.out.println("1000 records inserted into" + HASH_TABLE);
+	    	}
+		    
+	    	else if (this.type.equals("indexfile")) {
+	    		
+	    	}
+	    	else {
+	    		System.out.println("Incorrect db_test_option (should be caught in mainInterface");
+	    	}
+
+
+	
+
+		}
+        catch (Exception e1) {
+            System.err.println("Test failed: " + e1.toString());
+        }
+	}
+		
+	    static void populateTable(Database my_table, int nrecs ) {
+	    	int range;
+	            DatabaseEntry kdbt, ddbt;
+	    	String s;
+
+	    	/*  
+	    	 *  generate a random string with the length between 64 and 127,
+	    	 *  inclusive.
+	    	 *
+	    	 *  Seed the random number once and once only.
+	    	 */
+	    	Random random = new Random(1000000);
+
+	            try {
+	                for (int i = 0; i < nrecs; i++) {
+
+	    		/* to generate a key string */
+	    		range = 64 + random.nextInt( 64 );
+	    		s = "";
+	    		for ( int j = 0; j < range; j++ ) 
+	    		  s+=(new Character((char)(97+random.nextInt(26)))).toString();
+
+	    		/* to create a DBT for key */
+	    		kdbt = new DatabaseEntry(s.getBytes());
+	    		kdbt.setSize(s.length());
+	    		//System.out.println(kdbt);
+
+	                    // to print out the key/data pair
+	                     //System.out.println(kdbt.toString());	
+
+	    		/* to generate a data string */
+	    		range = 64 + random.nextInt( 64 );
+	    		s = "";
+	    		for ( int j = 0; j < range; j++ ) 
+	    		  s+=(new Character((char)(97+random.nextInt(26)))).toString();
+	                    // to print out the key/data pair
+	                    System.out.println(s);	
+	                    System.out.println("");
+	    		
+	    		/* to create a DBT for data */
+	    		ddbt = new DatabaseEntry(s.getBytes());
+	    		ddbt.setSize(s.length()); 
+
+	    		/* to insert the key/data pair into the database */
+	                    my_table.putNoOverwrite(null, kdbt, ddbt);
+	                }
+	            }
+	            catch (DatabaseException dbe) {
+	                System.err.println("Populate the table: "+dbe.toString());
+	                System.exit(1);
+	            }
+	        }
+		
+}
---
+++ src/mainInterface.java
@@ -0,0 +1,116 @@
+import com.sleepycat.db.*;
+
+import java.io.*;
+import java.util.*;
+
+public class mainInterface {
+
+
+	    /*
+	     *  the main function
+	     */
+	    public static void main(String[] args) throws FileNotFoundException, DatabaseException {
+
+	    	Scanner user_input = new Scanner( System.in );
+		    boolean option = false;
+		    Create c = null;
+		    while (option != true) {
+
+		    	System.out.println("Options:");
+		    	System.out.println("(1) Create and Populate a Database");
+		    	System.out.println("(2) Retrieve a Record with a given Key Value");
+		    	System.out.println("(3) Retrieve a Record with a given Data Value");
+		    	System.out.println("(4) Retrieve a list of Records with a given range of Key Values");
+		    	System.out.println("(5) Destroy the Database");
+		    	System.out.println("(6) Exit");
+
+		    	String type = user_input.next();
+
+		    	if (type.equals("1")) {
+		    		c = new Create(args[0]);
+		    		c.createDatabase();
+		    	}
+		    	else if (type.equals("2")) {
+		          //c.my_table.get(null, , data, lockMode)
+		    		
+		    		Cursor cursor = null;
+
+		    		    // Database and environment open omitted for brevity
+		    		  
+
+		    		    // Open the cursor. 
+		    		    cursor = c.my_table.openCursor(null, null);
+
+		    		    // Cursors need a pair of DatabaseEntry objects to operate. These hold
+		    		    // the key and data found at any given position in the database.
+		    		    DatabaseEntry foundKey = new DatabaseEntry();
+		    		    DatabaseEntry foundData = new DatabaseEntry();
+
+		    		    // To iterate, just call getNext() until the last database record has been 
+		    		    // read. All cursor operations return an OperationStatus, so just read 
+		    		    // until we no longer see OperationStatus.SUCCESS
+		    		    while (cursor.getNext(foundKey, foundData, LockMode.DEFAULT) ==
+		    		        OperationStatus.SUCCESS) {
+		    		        // getData() on the DatabaseEntry objects returns the byte array
+		    		        // held by that object. We use this to get a String value. If the
+		    		        // DatabaseEntry held a byte array representation of some other data
+		    		        // type (such as a complex object) then this operation would look 
+		    		        // considerably different.
+		    		        String keyString = new String(foundKey.getData());
+		    		        String dataString = new String(foundData.getData());
+		    		        System.out.println("Key | Data : " + keyString + " | " + 
+		    		                       dataString + "");
+		    		        System.out.println(c.my_table.get(null, foundKey, foundData, null).toString());
+		    		
+		    		
+		    		
+		    	}
+		    		    System.out.println("Enter key:");
+		    		    
+		    		     
+		    	}
+		    	else if (type.equals("3")) {
+		           
+		    	}
+		    	else if (type.equals("4")) {
+		    		
+		    	}
+		    	else if (type.equals("5")) {
+		    		if (c.type.equals("btree")) {
+		    		Database.remove(Create.BTREE_TABLE,null,null);
+		    		System.out.println("BTree database was destroyed");
+		    		}
+		    		else if (c.type.equals("hash")) {
+		    		Database.remove(Create.HASH_TABLE,null,null);
+		    		System.out.println("Hash database was destroyed");
+		    		}
+		    		else {
+		    			System.out.println("No database to destroy");
+		    		}
+		    	}
+		    	else if (type.equals("6")) {
+		    		option = true;
+		    		/* close the database and the db environment */
+				    //my_table.close();
+				    user_input.close();
+		    	}
+		    	else {
+		    		System.out.println("Incorrect option. Please try again");
+		    	}
+		    	}
+		    
+		    
+		    
+		    
+
+		    /* to remove the table */
+		    //Database.remove(SAMPLE_TABLE,null,null);
+	       
+	        }
+
+	    }
+	    
+
+	
+	
+
